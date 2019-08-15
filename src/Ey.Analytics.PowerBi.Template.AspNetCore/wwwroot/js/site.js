b95233fa6a1d6fdb8209bbda8eb35e4d4af1768a
thomas.mechelke@eymsprod.onmicrosoft.com <thomas.mechelke@eymsprod.onmicrosoft.com>
2019-08-15 14:07:46 -0400

    Update .gitignore to enable wwwroot static content

---
+++ src/Ey.Analytics.PowerBi.Template.AspNetCore/wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $("a").tooltip();
+
+});
+
+function displayTerms() {
+    $("#modal-1").modal();
+}
+
+function acceptTermsAndConditions() {
+
+    setTermsAcceptStatus("true");
+    displayReport();
+}
+
+function setTermsAcceptStatus(status) {
+    localStorage.setItem("TermsAndConditionsAccepted", "true");
+}
+function getTermsAcceptStatus() {
+    var status = localStorage.getItem("TermsAndConditionsAccepted");
+    return status;
+}
+
+function clearTermsAcceptStatus() {
+    localStorage.setItem("TermsAndConditionsAccepted", "false");
+}
+
